ffe6fbae4ec041f84368b71c7bee400ddeca7786
Hary Lala RABENAMANA <rabenamanaharylala2001@gmail.com>
2024-04-13 20:44:00 +0300

    seventh commit

--- src/ArithExpr.java
+++ src/ArithExpr.java
@@ -1,3 +1,4 @@
 public interface ArithExpr {
     double eval();
+    String toString();
 }
--- src/Main.java
+++ src/Main.java
@@ -19,16 +19,23 @@ public class Main {
 
         // Creation des expressions de constantes (essai)
         ArithExpr expression1 = new Addition(constant1, constant2);
-        ArithExpr expression2 = new Soustraction(constant1, constant2);
+        ArithExpr expression2 = new Soustraction(constant3, constant4);
+        ArithExpr expression3 = new Multiplication(constant5, constant6);
+        ArithExpr expression4 = new Division(constant7, constant8);
 
         // Creation de l'expression : 3 + (4.7 + 2.3) * 5
         ArithExpr expression = new Addition(A, new Multiplication(new Addition(B, C), D));
 
         // Affichage de l'expression (essai)
+        System.out.println("ESSAI");
         System.out.println(expression1 + " = " + expression1.eval());
         System.out.println(expression2 + " = " + expression2.eval());
+        System.out.println(expression3 + " = " + expression3.eval());
+        System.out.println(expression3 + " = " + expression4.eval());
 
         // Affichage de l'expression
+        System.out.println("");
+        System.out.println("OPÉRATION ARITHMÉTIQUE :");
         System.out.println(expression + " = " + expression.eval());
     }
 }
